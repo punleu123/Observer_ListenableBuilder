6536a481e680b607362a2340efcdcdb447c6edda
punleu123 <146927040+punleu123@users.noreply.github.com>
2026-02-21 13:54:42 +0700

    TASK-001 – The Color App - Q1-ChangeNotifier and ListenableBuilders

--- w4_practice/lib/1_color_app/color_app.dart
+++ w4_practice/lib/1_color_app/color_app.dart
@@ -11,6 +11,24 @@ void main() {
 
 enum CardType { red, blue }
 
+class TapCountNotifier extends ChangeNotifier {
+  int _redTapCount = 0;
+  int _blueTapCount = 0;
+
+  int get redTapCount => _redTapCount;
+  int get blueTapCount => _blueTapCount;
+
+  void incrementRedTap() {
+    _redTapCount++;
+    notifyListeners();
+  }
+
+  void incrementBlueTap() {
+    _blueTapCount++;
+    notifyListeners();
+  }
+}
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -20,36 +38,21 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
-  int redTapCount = 0;
-  int blueTapCount = 0;
 
-  void _incrementRedTapCount() {
-    setState(() {
-      redTapCount++;
-    });
-  }
+  final TapCountNotifier _tapCountNotifier = TapCountNotifier();
 
-  void _incrementBlueTapCount() {
-    setState(() {
-      blueTapCount++;
-    });
+  @override
+  void dispose() {
+    _tapCountNotifier.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-          _currentIndex == 0
-              ? ColorTapsScreen(
-                redTapCount: redTapCount,
-                blueTapCount: blueTapCount,
-                onRedTap: _incrementRedTapCount,
-                onBlueTap: _incrementBlueTapCount,
-              )
-              : StatisticsScreen(
-                redTapCount: redTapCount,
-                blueTapCount: blueTapCount,
-              ),
+      body: _currentIndex == 0
+          ? ColorTapsScreen(notifier: _tapCountNotifier)
+          : StatisticsScreen(notifier: _tapCountNotifier),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
@@ -73,31 +76,31 @@ class _HomeState extends State<Home> {
 }
 
 class ColorTapsScreen extends StatelessWidget {
-  final int redTapCount;
-  final int blueTapCount;
-  final VoidCallback onRedTap;
-  final VoidCallback onBlueTap;
+  final TapCountNotifier notifier;
 
-  const ColorTapsScreen({
-    super.key,
-    required this.redTapCount,
-    required this.blueTapCount,
-    required this.onRedTap,
-    required this.onBlueTap,
-  });
+  const ColorTapsScreen({super.key, required this.notifier});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Color Taps')),
-      body: Column(
-        children: [
-          ColorTap(type: CardType.red, tapCount: redTapCount, onTap: onRedTap),
-          ColorTap(
-            type: CardType.blue,
-            tapCount: blueTapCount,
-            onTap: onBlueTap,
-          ),
-        ],
+      body: ListenableBuilder(
+        listenable: notifier,
+        builder: (context, child) {
+          return Column(
+            children: [
+              ColorTap(
+                type: CardType.red,
+                tapCount: notifier.redTapCount,
+                onTap: notifier.incrementRedTap,
+              ),
+              ColorTap(
+                type: CardType.blue,
+                tapCount: notifier.blueTapCount,
+                onTap: notifier.incrementBlueTap,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
@@ -141,27 +144,33 @@ class ColorTap extends StatelessWidget {
 }
 
 class StatisticsScreen extends StatelessWidget {
-  final int redTapCount;
-  final int blueTapCount;
+  final TapCountNotifier notifier;
 
-  const StatisticsScreen({
-    super.key,
-    required this.redTapCount,
-    required this.blueTapCount,
-  });
+  const StatisticsScreen({super.key, required this.notifier});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Statistics')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Red Taps: $redTapCount', style: TextStyle(fontSize: 24)),
-            Text('Blue Taps: $blueTapCount', style: TextStyle(fontSize: 24)),
-          ],
-        ),
+      body: ListenableBuilder(
+        listenable: notifier,
+        builder: (context, child) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Red Taps: ${notifier.redTapCount}',
+                  style: TextStyle(fontSize: 24),
+                ),
+                Text(
+                  'Blue Taps: ${notifier.blueTapCount}',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
