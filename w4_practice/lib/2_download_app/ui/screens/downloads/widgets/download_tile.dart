fcc145eb89ead2f775ef106e22ca32d212b603d7
punleu123 <146927040+punleu123@users.noreply.github.com>
2026-02-22 16:41:03 +0700

    TASK-002 – Downloader – Downloads

--- w4_practice/lib/2_download_app/ui/screens/downloads/widgets/download_tile.dart
+++ w4_practice/lib/2_download_app/ui/screens/downloads/widgets/download_tile.dart
@@ -1,18 +1,57 @@
 import 'package:flutter/material.dart';
- 
+
 import 'download_controler.dart';
 
 class DownloadTile extends StatelessWidget {
   const DownloadTile({super.key, required this.controller});
 
   final DownloadController controller;
- 
- // TODO
+
+  // TODO
+  Widget _buildTrailing(DownloadStatus status) {
+    switch (status) {
+      case DownloadStatus.notDownloaded:
+        return IconButton(
+          onPressed: controller.startDownload,
+          icon: const Icon(Icons.download),
+        );
+      case DownloadStatus.downloading:
+        return Icon(Icons.downloading_rounded);
+      case DownloadStatus.downloaded:
+        return const Icon(Icons.folder);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
-     
+    return ListenableBuilder(
+      listenable: controller,
+      builder: (BuildContext context, Widget? child) {
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: ListTile(
+                title: Text(controller.ressource.name),
+                subtitle:
+                    controller.status == DownloadStatus.downloading ||
+                        controller.status == DownloadStatus.downloaded
+                    ? Text(
+                        '${(controller.progress * 100)}% Completed - ${controller.progress * controller.ressource.size} of ${controller.ressource.size} MB',
+                      )
+                    : Text(""),
+                trailing: _buildTrailing(controller.status),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+
     // TODO
   }
 }
